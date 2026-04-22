e6c96448c5dc2d2a2ec2dc1dbd39e909943067da
USIA <gh.ziwo.129@gmail.com>
2026-04-22 11:35:09 +0000

    5th-time

---
+++ main6.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("value = %v\n", float32(1.0000000000000000))
+    fmt.Println("value = %v\n", float32(1.0000000000000001))
+    fmt.Println("value = %v\n", float32(1.0000000000000002))
+    fmt.Println("value = %v\n", float32(1.0000000000000003))
+    fmt.Println("value = %v\n", float32(1.0000000000000004))
+    fmt.Println("value = %v\n", float32(1.0000000000000005))
+}
---
+++ main7.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+       a := [5]int{1, 2, 3, 4, 5}
+       fmt.Printf("%v\n", a)
+       fmt.Println(a[0])
+       fmt.Println(a[1])
+       fmt.Println(a[2])
+       fmt.Println(a[3])
+       fmt.Println(a[4])
+
+}
